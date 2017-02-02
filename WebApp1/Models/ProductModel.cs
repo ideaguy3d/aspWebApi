2719746db6507c5be9d5a6757772061699c694b4
Julius Hernandez Alvarado <JHernandezAlvarado@sjcoe.net>
2017-02-01 17:01:41 -0800

    added git

---
+++ WebApp1/Models/ProductModel.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations; 
+
+namespace WebApp1.Models
+{
+    public class ProductModel
+    {
+        public string Description { get; set; }
+
+        public decimal Price { get; set; }
+
+        [Required(ErrorMessage ="ProductCode is required", AllowEmptyStrings =false)]
+        [MinLength(6, ErrorMessage = "Product code min length is 6 characters")]
+        public string ProductCode { get; set; }
+
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage ="Product Name is required", AllowEmptyStrings = false)]
+        [MinLength(5, ErrorMessage = "Product Name min length is 5 chars")]
+        [MaxLength(11, ErrorMessage ="Product Name max length is 11 chars")]
+        public string ProductName { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+    }
+}
---
+++ WebApp1/Models/ProductRepo.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json; 
+using System.Web.Hosting; 
+
+namespace WebApp1.Models
+{
+    /// <summary>
+    /// This class will store data in a local json file.
+    /// </summary>
+    public class ProductRepo
+    {
+        /// <summary>
+        /// Creates a new Product w/default vals.  
+        /// </summary>
+        /// <returns></returns>
+        internal ProductModel Create()
+        {
+            var product = new ProductModel { ReleaseDate = DateTime.Now };
+            return product; 
+        }
+
+        /// <summary>
+        /// Returns all the data from the local json file.  
+        /// </summary>
+        /// <returns></returns>
+        internal List<ProductModel> Retrieve()
+        {
+            var filePath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
+            var json = System.IO.File.ReadAllText(filePath);
+            var products = JsonConvert.DeserializeObject<List<ProductModel>>(json);
+            return products; 
+        }
+
+        /// <summary>
+        /// POSTS a product to the local json file.  
+        /// </summary>
+        /// <params name="product"></params>
+        /// <returns></returns>
+        internal ProductModel Save(ProductModel product)
+        {
+            var products = this.Retrieve();
+            var maxId = products.Max(p => p.ProductId);
+            product.ProductId = maxId + 1;
+            products.Add(product);
+            WriteData(products);
+            return product; 
+        }
+
+        /// <summary>
+        /// PUTS a product to the local json file.  
+        /// </summary>
+        /// <params name="product"></params>
+        /// <params name="productId"></params>
+        /// <returns></returns>
+        internal ProductModel Save(ProductModel product, int productId)
+        {
+            var products = this.Retrieve();
+            var itemIndex = products.FindIndex(p => p.ProductId == product.ProductId);
+            if (itemIndex > 0) products[itemIndex] = product;
+            else return null;
+
+            WriteData(products);
+            return product; 
+        }
+
+        private bool WriteData(List<ProductModel> products)
+        {
+            // write out the json
+            var filePath = HostingEnvironment.MapPath(@"~/App_Data/product.json");
+
+            var json = JsonConvert.SerializeObject(products, Formatting.Indented);
+            System.IO.File.WriteAllText(filePath, json);
+
+            return true; 
+        }
+    }
+}
